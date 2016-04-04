2b2b197c7dd3fccca87f8e3e87dda45cfdef9b7e
grendizer <grendizer@163.com>
2016-04-05 03:12:38 +0800

    删除不必要的方法

--- src/HeaderBag.php
+++ src/HeaderBag.php
@@ -6,7 +6,6 @@ namespace Grendizer\HttpMessage;
 class HeaderBag implements BagInterface
 {
     protected $headers = array();
-    protected $cacheControl = array();
 
     /**
      * Special HTTP headers that do not have the "HTTP_" prefix
@@ -93,6 +92,11 @@ class HeaderBag implements BagInterface
      */
     public function set($key, $value, $replace = true)
     {
+        if (empty($value)) {
+            $this->remove($key);
+            return;
+        }
+        
         if (!is_array($value)) {
             $value = array($value);
         }
@@ -102,17 +106,11 @@ class HeaderBag implements BagInterface
         if ($replace && !empty($oldValue) && is_array($oldValue)) {
             $value = array_replace($oldValue, $value);
         }
-        
-        $normalizeKey = $this->normalizeKey($key);
 
-        $this->headers[$normalizeKey] = array(
+        $this->headers[$this->normalizeKey($key)] = array(
             'value' => $value,
             'originalKey' => $key
         );
-
-        if ('cache-control' === $normalizeKey) {
-            $this->cacheControl = $this->parseCacheControl($value[0]);
-        }
     }
 
     /**
@@ -196,10 +194,6 @@ class HeaderBag implements BagInterface
     public function remove($key)
     {
         unset($this->headers[$key = $this->normalizeKey($key)]);
-        
-        if ('cache-control' === $key) {
-            $this->cacheControl = array();
-        }
     }
 
     /**
@@ -255,77 +249,6 @@ class HeaderBag implements BagInterface
         return filter_var($value, $filter, $options);
     }
 
-    /**
-     * Returns the HTTP header value converted to a date.
-     *
-     * @param string    $key     The parameter key
-     * @param \DateTime $default The default value
-     *
-     * @return null|\DateTime The parsed DateTime or the default value if the header does not exist
-     *
-     * @throws \RuntimeException When the HTTP header is not parseable
-     */
-    public function getDate($key, \DateTime $default = null)
-    {
-        if (null === $value = $this->get($key)) {
-            return $default;
-        }
-
-        if (false === $date = \DateTime::createFromFormat(DATE_RFC2822, $value)) {
-            throw new \RuntimeException(sprintf('The %s HTTP header is not parseable (%s).', $key, $value));
-        }
-
-        return $date;
-    }
-
-    /**
-     * Adds a custom Cache-Control directive.
-     *
-     * @param string $key   The Cache-Control directive name
-     * @param mixed  $value The Cache-Control directive value
-     */
-    public function addCacheControlDirective($key, $value = true)
-    {
-        $this->cacheControl[$key] = $value;
-
-        $this->set('Cache-Control', $this->getCacheControlHeader());
-    }
-
-    /**
-     * Returns true if the Cache-Control directive is defined.
-     *
-     * @param string $key The Cache-Control directive
-     *
-     * @return bool true if the directive exists, false otherwise
-     */
-    public function hasCacheControlDirective($key)
-    {
-        return array_key_exists($key, $this->cacheControl);
-    }
-
-    /**
-     * Returns a Cache-Control directive value by name.
-     *
-     * @param string $key The directive name
-     *
-     * @return mixed|null The directive value if defined, null otherwise
-     */
-    public function getCacheControlDirective($key)
-    {
-        return array_key_exists($key, $this->cacheControl) ? $this->cacheControl[$key] : null;
-    }
-
-    /**
-     * Removes a Cache-Control directive.
-     *
-     * @param string $key The Cache-Control directive
-     */
-    public function removeCacheControlDirective($key)
-    {
-        unset($this->cacheControl[$key]);
-
-        $this->set('Cache-Control', $this->getCacheControlHeader());
-    }
 
     /**
      * Returns an iterator for parameters.
@@ -345,42 +268,5 @@ class HeaderBag implements BagInterface
     public function count()
     {
         return count($this->headers);
-    }
-    
-    protected function getCacheControlHeader()
-    {
-        $parts = array();
-        ksort($this->cacheControl);
-        foreach ($this->cacheControl as $key => $value) {
-            if (true === $value) {
-                $parts[] = $key;
-            } else {
-                if (preg_match('#[^a-zA-Z0-9._-]#', $value)) {
-                    $value = '"'.$value.'"';
-                }
-
-                $parts[] = "$key=$value";
-            }
-        }
-
-        return implode(', ', $parts);
-    }
-
-    /**
-     * Parses a Cache-Control HTTP header.
-     *
-     * @param string $header The value of the Cache-Control HTTP header
-     *
-     * @return array An array representing the attribute values
-     */
-    protected function parseCacheControl($header)
-    {
-        $cacheControl = array();
-        preg_match_all('#([a-zA-Z][a-zA-Z_-]*)\s*(?:=(?:"([^"]*)"|([^ \t",;]*)))?#', $header, $matches, PREG_SET_ORDER);
-        foreach ($matches as $match) {
-            $cacheControl[strtolower($match[1])] = isset($match[3]) ? $match[3] : (isset($match[2]) ? $match[2] : true);
-        }
-
-        return $cacheControl;
     }
 }
